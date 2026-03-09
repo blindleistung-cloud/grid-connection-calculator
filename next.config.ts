189f8ba5acf7d79ff42cba62ccb312a9b4366f00
Riccardo Dreißas <Riccardo.Dreissas@gmail.com>
2026-03-09 14:11:08 +0000

    the github action workflow went well, but the content does not look like

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/grid-connection-calculator",
   images: {
     unoptimized: true,
     remotePatterns: [
